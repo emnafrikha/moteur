1896203c364c22b9b95e130a968914389e0bb138
ruminaaa <amina.dhieb@etudiant-enit.utm.tn>
2024-05-10 23:07:36 +0100

    update4

--- src/Scoreur.java
+++ src/Scoreur.java
@@ -1,3 +1,15 @@
-public class Scoreur {
+import java.util.Map;
+import java.util.List;
 
-}
+public class Scoreur{
+ private double calculerScore(Map<String, Integer> occurrences, List<String> requete) {
+    double score = 0;
+    int totalMotsRequete = requete.size();
+
+    for (String mot : requete) {
+        int occurrenceMotDansFichier = occurrences.getOrDefault(mot, 0);
+        score += (double) occurrenceMotDansFichier / totalMotsRequete;
+    }
+
+    return score;
+}}
